68c515d00dc17546c3d32ae48e14bde7b38faa43
Ricardo Arias <reariast128@proton.me>
2024-03-20 12:42:59 -0500

    First commit.

---
+++ NoteApp/Nota.cs
@@ -0,0 +1,19 @@
+namespace NoteApp;
+
+public class Nota
+{
+    public DateTime horaCreacion;
+    public DateTime horaModificacion;
+    public string titulo;
+    public string contenido;
+
+    public Nota(string titulo, string contenido)
+    {
+        this.titulo = titulo;
+        this.contenido = contenido;
+        this.horaCreacion = DateTime.Now;
+        this.horaModificacion = DateTime.Now;
+    }
+    
+    
+}
