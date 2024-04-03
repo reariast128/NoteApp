b395533b58f38f7f4330a2b5ece2da3bbdcd21a5
Ricardo Arias <reariast128@proton.me>
2024-04-03 13:07:02 -0500

    Ahora se viene lo bueno.

--- NoteApp/Controlador.cs
+++ NoteApp/Controlador.cs
@@ -2,5 +2,16 @@ namespace NoteApp;
 
 public class Controlador
 {
-    
+    public Usuario usuarioActual;
+    public Gui gui;
+
+    public Controlador(Usuario usuario)
+    {
+        this.usuarioActual = usuario;
+    }
+
+    public void CrearCuaderno(string nombre)
+    {
+        this.usuarioActual.crearCuaderno(nombre);
+    }
 }
