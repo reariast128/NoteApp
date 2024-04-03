43c27b452a3fd45dee75d9400077233e9c18d1ad
Ricardo Arias <reariast128@proton.me>
2024-04-03 17:32:09 -0500

    Creo que ya añadí todos los setters y getters requeridos.

--- NoteApp/Controlador.cs
+++ NoteApp/Controlador.cs
@@ -1,14 +1,15 @@
+using System.Numerics;
+using System.Runtime.InteropServices.JavaScript;
+
 namespace NoteApp;
 
 public class Controlador
 {
     public Usuario usuarioActual;
-    public Gui gui;
 
-    public Controlador(Usuario usuario, Gui gui)
+    public Controlador(Usuario usuario)
     {
         this.usuarioActual = usuario;
-        this.gui = gui;
     }
 
     public void CrearCuaderno(string nombre)
@@ -26,7 +27,7 @@ public class Controlador
         this.usuarioActual.cuadernos[idCuaderno].modificarTitulo(titulo);
     }
 
-    public void CrearNota(int idCuaderno, string tituloNota,string contenidoNota)
+    public void CrearNota(int idCuaderno, string tituloNota, string contenidoNota)
     {
         this.usuarioActual.cuadernos[idCuaderno].crearNota(tituloNota, contenidoNota);
     }
@@ -45,5 +46,45 @@ public class Controlador
     {
         this.usuarioActual.cuadernos[idCuaderno].modificarTituloNota(idNota, titulo);
     }
+
+    public string ObtenerNombreUsuario => this.usuarioActual.nombre;
+
+    public List<string> ObtenerTitulosCuaderno(int idCuaderno)
+    {
+        List<string> titulosCuadernos = new List<string>();
+
+        foreach (var cuaderno in this.usuarioActual.cuadernos)
+        {
+            titulosCuadernos.Append(cuaderno.titulo);
+        }
+
+        return titulosCuadernos;
+    }
+
+    public List<String> ObtenerTitulosNotasCuaderno(int idCuaderno)
+    {
+        List<String> titulosNotas = new List<string>();
+
+        foreach (var nota in this.usuarioActual.cuadernos[idCuaderno].notas)
+        {
+            titulosNotas.Append(nota.titulo);
+        }
+
+        return titulosNotas;
+    }
+
+    public DateTime ObtenerFechaCreacionCuaderno(int idCuaderno) =>
+        this.usuarioActual.cuadernos[idCuaderno].horaCreacion;
     
+    public DateTime ObtenerFechaModificacionCuaderno(int idCuaderno) =>
+        this.usuarioActual.cuadernos[idCuaderno].horaModificacion;
+    
+    public string ObtenerContenidoNota(int idCuaderno, int idNota) =>
+        this.usuarioActual.cuadernos[idCuaderno].notas[idNota].contenido;
+    
+    public DateTime ObtenerFechaCreacionNota(int idCuaderno, int idNota) =>
+        this.usuarioActual.cuadernos[idCuaderno].notas[idNota].horaCreacion;
+    
+    public DateTime ObtenerFechaModificacionNota(int idCuaderno, int idNota) =>
+        this.usuarioActual.cuadernos[idCuaderno].notas[idNota].horaModificacion;
 }
